65ab65ba633a86c013d07bc8bea2c51b959c2ab4
Kacperrk <130840734+Kacperrk@users.noreply.github.com>
2025-01-23 09:13:12 +0100

    Fixed config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost:3001",
-    baseURL:"http://10.0.2.2:3001",
+    // baseURL:"http://localhost:3001",
+    // baseURL:"http://10.0.2.2:3001",
     baseURL:"http://192.168.30.107:3001",
-    timeout: 1000,
+    timeout: 1000
 });
 
 export default API;
